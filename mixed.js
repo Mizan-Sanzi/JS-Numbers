30c3fb94197feadf5f5d96de8bb35109ad51c230
mizansanzi <pleasemailmizan@gmail.com>
2024-08-31 17:40:07 +0600

    first commit

---
+++ mixed.js
@@ -0,0 +1,10 @@
+var first = "Mizan";
+var second = "Rahman";
+var fullName = first + second;
+console.log (fullName);
+
+console.log (40 + "Brothers");
+
+// concate
+
+console.log (40+"30");
---
+++ numbers.js
@@ -0,0 +1,25 @@
+
+// variable and etc
+
+var first = 2.3;
+var second = 1.5;
+var total = first + second;
+
+console.log (total);
+console.log (total.toFixed(3));
+
+// shorthand
+
+var price = 34;
+var newPrice = price + 10;
+newPrice += 45;
+
+console.log (newPrice);
+
+var fridge = 20000;
+fridge -= 1000;
+
+console.log (fridge);
+
+var foodPrice = price*10;
+console.log (foodPrice);
